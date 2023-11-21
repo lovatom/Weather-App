29971f69b2847c6e40682fadbea35e7a21cc7c86
lovatom <lovato.maxence@gmail.com>
2023-11-21 20:14:12 +0100

    updated application

--- app.js
+++ app.js
@@ -14,3 +14,23 @@ app.get('', (req, res) => {
 app.listen(3000, function(){
     console.log('Server is up at port 3000');
 });
+
+app.get('/getWeather', async (req, res) => {
+  try {
+    const apiKey = process.env.OPEN_WEATHER_API_KEY;
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather'; // Example API endpoint
+    const params = {
+      q: req.query.city, // Assuming the city is sent as a query parameter
+      appid: apiKey,
+    };
+
+    const response = await axios.get(apiUrl, { params });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
